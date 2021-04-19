15fdfbe23be8e90444ea6b58cf85e37fd0f1fafb
jakob simonsen <jakob.simonsen@outlook.com>
2021-04-19 10:18:35 +0200

    init mqtt client and audio - need to be made to components

---
+++ main.py
@@ -0,0 +1,11 @@
+import paho.mqtt.client as mqtt
+from mqtt_client import MQTT_Client
+from recorder import Recorder
+
+#setting public broker
+broker, port = 'mqtt.item.ntnu.no', 1883
+
+client = MQTT_Client()
+client.start(broker, port)
+
+recorder = Recorder()
