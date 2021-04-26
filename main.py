30de2539ad86976368ce5e531052bf2204085bcd
Toni Vucic <tonivuc@gmail.com>
2021-04-26 15:03:49 +0200

    Fikset imports og requirement.txt sånn at koden kjører

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 from mqtt_client import MQTT_Client
-from recorder import Recorder
+from Audio.recorder import Recorder
 
 #setting public broker
 broker, port = 'mqtt.item.ntnu.no', 1883
