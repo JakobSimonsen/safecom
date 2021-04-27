d7be8092fa4d7216e3ce587a1fe6b33e7ca123b3
Hedda Langvik <hedda.langvik@gmail.com>
2021-04-27 12:38:47 +0200

    updated gui with priority

--- coordinator_stm.py
+++ coordinator_stm.py
@@ -5,6 +5,7 @@ from Audio import recorder, playback
 from stmpy import Driver, Machine
 from threading import Thread
 
+
 class Coordinator:
     def __init__(self):
         self.channel = None
@@ -12,36 +13,37 @@ class Coordinator:
         self.client = MQTT_Client()
         self.client.start('mqtt.item.ntnu.no', 1883)
 
-        #self.voice_msg??
+        # self.voice_msg??
 
     def start_recording(self):
         print("starting recording")
-        #self.recorder.record()
+        # self.recorder.record()
         self.stm_driver.send("start", "recorder_stm")
 
-    def end_recording(self):
+    def end_recording(self, priority):
         print("ending recording")
-        #self.recorder.stop()
-        #self.recorder.process()
+        self.priority = 1 if priority else 0
+        # self.recorder.stop()
+        # self.recorder.process()
         self.stm_driver.send("stop", "recorder_stm")
 
     def play_msg(self):
         print("playing message")
-        #self.player.play()
-        #??
+        # self.player.play()
+        # ??
         self.stm_driver.send("start", "playback_stm")
         self.stm_driver.send("done", "playback_stm")
 
     def send_msg(self):
         print("sending message")
-        #?? send(s)
-        self.client.client.publish(self.channel, "message")
+        # ?? send(s)
+        self.client.client.publish(
+            self.channel, str("message" + str(self.priority)))
         #self.client.publish_recorded_message(self.channel, self.priority, filename)
-
 
     def set_new_channel(self, new_channel):
         print("setting new channel")
-        #Get chosen channel on gui
+        # Get chosen channel on gui
         if self.channel != None:
             self.client.client.unsubscribe(self.channel)
         self.channel = new_channel
@@ -53,60 +55,61 @@ coordinator = Coordinator()
 t0 = {'source': 'initial',
       'target': 'idle'}
 
-t1 = {'trigger':'record_button',
+t1 = {'trigger': 'record_button',
       'source': 'idle',
       'target': 'recording'}
 
-t2 = {'trigger':'new_incoming_msg',
+t2 = {'trigger': 'new_incoming_msg',
       'source': 'idle',
       'target': 'playing'}
 
-t3 = {'trigger':'play_from_history',
+t3 = {'trigger': 'play_from_history',
       'source': 'idle',
       'target': 'playing'}
 
-t4 = {'trigger':'t3',
+t4 = {'trigger': 't3',
       'source': 'recording',
       'target': 'saving_file'}
 
-t5 = {'trigger':'end_recording_button',
+t5 = {'trigger': 'end_recording_button',
       'source': 'recording',
-      'target': 'saving_file'}
+      'target': 'saving_file',
+      'effect': 'end_recording(*)'}
 
-
-t6 = {'trigger':'file_saved',
+t6 = {'trigger': 'file_saved',
       'source': 'saving_file',
       'target': 'sending'}
 
-t7 = {'trigger':'t1',
+t7 = {'trigger': 't1',
       'source': 'playing',
       'target': 'idle'}
 
-t8 = {'trigger':'stop_button',
+t8 = {'trigger': 'stop_button',
       'source': 'playing',
       'target': 'idle'}
 
-t9 = {'trigger':'sending_failed',
+t9 = {'trigger': 'sending_failed',
       'source': 'sending',
       'target': 'saving_file'}
 
-t10 = {'trigger':'sending_success',
-      'source': 'sending',
-      'target': 'idle'}
-t11 = {'trigger':'fileSaved',
-      'source': 'saving_file',
-      'target': 'sending'}
+t10 = {'trigger': 'sending_success',
+       'source': 'sending',
+       'target': 'idle'}
+
+t11 = {'trigger': 'fileSaved',
+       'source': 'saving_file',
+       'target': 'sending'}
 
 
 idle = {'name': 'idle', 'change_channel': 'set_new_channel(*)'}
 
 recording = {'name': 'recording',
              'entry': 'start_recording; start_timer("t3", 180000); ',
-             'exit': 'end_recording',
+
              'new_incoming_msg': 'defer'}
 
 saving_file = {'name': 'saving_file',
-                 'new_incoming_msg': 'defer'}
+               'new_incoming_msg': 'defer'}
 
 sending = {'name': 'sending',
            'entry': 'send_msg',
@@ -117,7 +120,8 @@ playing = {'name': 'playing',
            'entry': 'start_timer("t1", 10000); play_msg',
            'new_incoming_msg': 'defer'}
 
-machine = Machine(name='coordinator', transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11], obj=coordinator, states=[idle, recording, saving_file, playing, sending])
+machine = Machine(name='coordinator', transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11], obj=coordinator, states=[
+                  idle, recording, saving_file, playing, sending])
 coordinator.stm = machine
 
 driver = Driver()
@@ -131,16 +135,7 @@ driver.start()
 coordinator.stm_driver = driver
 playback.player.stm_driver = driver
 
-#Just used to test the recorder.py atm -Toni
+# Just used to test the recorder.py atm -Toni
 #driver.send("start", "recorder_stm")
-#time.sleep(3)
+# time.sleep(3)
 #driver.send("stop", "recorder_stm")
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -4,21 +4,25 @@ import coordinator_stm
 
 with open('./channels.json') as f:
     channel_values = json.load(f)
-    print(channel_values['channel1']['name'])
 
 
 title = [sg.Text('This is your new awesome walkie talki')]
 messages = [sg.Listbox(values=["Message 1", "Message 2"], size=(30, 10))]
-channels = [sg.Combo([channel_values[v]['name'] for v in channel_values],
-                     enable_events=True, key='channels', size=(15, 1))]
-channel_button = [sg.Button('Update', size=(10,1), key='Update')]
+
+channels = sg.Combo([channel_values[v]['name'] for v in channel_values],
+                    enable_events=True, key='channels', size=(15, 1))
+channel_button = sg.Button('Update', size=(10, 1), key='Update')
+
+priority = [sg.Checkbox('Priority', key="Priority")]
 record_button = [sg.Button('Record', size=(25, 1), key='Record', visible=True)]
-stop_button = [sg.Button('Stop and Send', size=(25, 1), key='Stop', visible=False)]
+stop_button = [sg.Button('Stop and Send', size=(25, 1),
+                         key='Stop', visible=False)]
 
 # All the stuff inside your window.
 layout = [title,
           messages,
           [channels, channel_button],
+          priority,
           record_button,
           stop_button]
 
@@ -34,23 +38,25 @@ while True:
     if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
         break
 
-    if event == 'Record':
+    elif event == 'Record':
         print('recording')
         coordinator_stm.driver.send("record_button", "coordinator")
         window['Record'].Update(visible=False)
         window['Stop'].Update(visible=True)
 
-    if event == 'Stop':
+    elif event == 'Stop':
         print('stop recording')
-        coordinator_stm.driver.send("end_recording_button", "coordinator")
+        coordinator_stm.driver.send(
+            "end_recording_button", "coordinator", args=[values['Priority']])
         window['Record'].Update(visible=True)
         window['Stop'].Update(visible=False)
 
-    if event == 'Update':
+    elif event == 'Update':
         channel_name = values['channels']
         for channel in channel_values.values():
             if channel['name'] == channel_name:
-                coordinator_stm.driver.send("change_channel", "coordinator", args=[channel['topic']])
+                coordinator_stm.driver.send(
+                    "change_channel", "coordinator", args=[channel['topic']])
 
-
+coordinator_stm.driver.stop()
 window.close()
