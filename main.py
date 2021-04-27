c1bb83e471f006571e12fb1e368ecc354c6843b9
jakob simonsen <jakob.simonsen@outlook.com>
2021-04-27 08:43:53 +0200

    stash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 from mqtt_client import MQTT_Client
-from recorder import Recorder
+from .recorder import Recorder
 
 #setting public broker
 broker, port = 'mqtt.item.ntnu.no', 1883
@@ -8,4 +8,4 @@ broker, port = 'mqtt.item.ntnu.no', 1883
 client = MQTT_Client()
 client.start(broker, port)
 
-recorder = Recorder()
+recording = Recorder()
